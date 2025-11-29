aecbf64b31e2a1b09d4127f3cc71a077645dcc8e
Museus <69139347+Museus@users.noreply.github.com>
2025-11-28 23:23:13 -0500

    feat: support ReactionWatchers (#7)

--- src/versecbot/client.py
+++ src/versecbot/client.py
@@ -1,6 +1,15 @@
 import discord
 
-intents = discord.Intents.default()
-intents.message_content = True
 
-client = discord.Client(intents=intents)
+class DiscordClient:
+    def __init__(self):
+        self.client = None
+
+    def initialize(self, plugin_intents):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        intents.reactions = True
+        self.client = discord.Client(intents=intents)
+
+
+discord_client = DiscordClient()
